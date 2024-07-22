96741d227341d22b13fc09c565a063f300103a41
Sevelantis <34632434+Sevelantis@users.noreply.github.com>
2024-07-22 19:19:08 +0200

    Update heic_to_jpg.sh

--- heic_to_jpg.sh
+++ heic_to_jpg.sh
@@ -1,23 +1,38 @@
-#!/bin/bash -eu
+#!/bin/bash -e
 
+# ::INFO::
 # This script converts all *.heic files inside a directory to .jpg files 
 # How to run: bash heic_to_jpg.sh my-directory-with-heic-files
 
-DIR=$1
-if [ -z $DIR ]; then
-    echo "Usage: $0 directory_to_convert"
+# read first argument
+DIR="${1}"
+
+# check if first argument is provided
+if [ -z ${DIR} ]; then
+    echo "How to run: '$0 my-directory-with-heic-files'"
     exit 1
 fi
 
-if ! find . -maxdepth 1 -type d -name $DIR | grep -q '.'; then
-  echo "Directory '$DIR' does not exist."
+# check if directory provided is located in the same directory as the script
+if ! find . -maxdepth 1 -type d -name ${DIR} | grep -q '.'; then
+  echo "Directory '${DIR}' does not exist."
   exit 1
 fi
 
-find $DIR -type f -iname '*.heic' | while read i; do \
-  fileNoExt="${i%.*}"; \
-  jpgFile="${fileNoExt}_heic_conv.jpg"; \
-  sips -s format jpeg -s formatOptions best "$i" --out "$jpgFile"; \
-  touch -r "$i" "$jpgFile"; \
-  rm "$i"; \
+# Find .heic files in provdided directory and convert them to .jpg
+find "${DIR}" -type f -iname '*.heic' | while read -r heicFile; do
+  # Remove the file extension
+  fileWithoutExt="${heicFile%.*}"
+  
+  # Set the new file name with .jpg extension
+  jpgFile="${fileWithoutExt}_heic_conv.jpg"
+  
+  # Convert the file to jpg format
+  sips -s format jpeg -s formatOptions best "$heicFile" --out "$jpgFile"
+  
+  # Set the timestamp of the new file to match the original file
+  touch -r "$heicFile" "$jpgFile"
+  
+  # Remove the original .heic file
+  rm "$heicFile"
 done
